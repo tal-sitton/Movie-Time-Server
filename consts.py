04ebffade2f2b6c67694acb0108f24eca88d85b1
Tal <tal.sitton04@gmail.com>
2022-09-04 23:40:10 +0300

    added rav hen api and removed selenium usage

--- consts.py
+++ consts.py
@@ -1,28 +1,5 @@
 from enum import Enum
 
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
-
-chrome_options = webdriver.ChromeOptions()
-
-options = [
-    "--headless",
-    "--disable-gpu",
-    "--window-size=1920,1200",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage",
-    "--blink-settings=imagesEnabled=false"
-]
-for option in options:
-    chrome_options.add_argument(option)
-
-driver_path = ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-driver = webdriver.Chrome(driver_path, options=chrome_options)
-driver.implicitly_wait(10)
-
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
 }
--- rav_hen.py
+++ rav_hen.py
@@ -1,12 +1,9 @@
-import time
 from enum import Enum
 
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
 
 from Screening import Screening
-from consts import driver, movies, MovieType, Districts
+from consts import movies, MovieType, Districts
 
 
 class Locations(Enum):
@@ -35,44 +32,32 @@ class Locations(Enum):
     }
 
 
-def prepare(location: Locations, date: str):
-    url = f"https://www.rav-hen.co.il/#/buy-tickets-by-cinema?in-cinema={location.value['code']}&at={date}&view-mode=list"
-    driver.get(url)
-    driver.refresh()
-    time.sleep(1)
-    if driver.current_url != url:
-        print("NO DATE FOUND")
-        return []
-    wait = WebDriverWait(driver, 10)
-    wait.until(EC.element_to_be_clickable((By.CLASS_NAME, 'qb-movie-name')))
-    return driver.find_elements(By.CLASS_NAME, "qb-movie")
+def prepare(location: Locations, date: str, s: requests.Session) -> tuple:
+    url = f"https://www.rav-hen.co.il/rh/data-api-service/v1/quickbook/10104/film-events/in-cinema/{location.value['code']}/at-date/{date}"
+    res = s.get(url)
+    data = dict(res.json().get("body"))
+    movies_ids = {movie.get("id"): movie.get("name") for movie in data.get("films")}
+    return movies_ids, data.get("events")
 
 
-def get_by_location(location: Locations, date: str, format_date: str):
+def get_by_location(location: Locations, date: str, format_date: str, s: requests.Session):
     print("STARTED RAV HEN ", location.name)
-    movies_data = prepare(location, date)
-    for movie in movies_data:
-        movie_name = movie.find_element(By.CLASS_NAME, "qb-movie-name").text.replace("עם כתוביות", "").replace("עברית",
-                                                                                                               "").strip()
-        all_data = movie.find_elements(By.CLASS_NAME, "qb-movie-info-column")
-        for type_data in all_data:
-            data = type_data.text
-            for info in type_data.find_elements(By.TAG_NAME, "a"):
-                link = info.get_attribute("data-url")
-                if not link:
-                    continue
-                m_time = info.text
-                movies.append(
-                    Screening(format_date, "רב חן", location.value['name'], location.value['dis'], movie_name,
-                              MovieType.m_2D, m_time, link)
-                )
+    movies_ids, events = prepare(location, date, s)
+    for event in events:
+        movie_name = movies_ids.get(event.get("filmId"))
+        m_time = ":".join(event.get("eventDateTime").split("T")[1].split(":")[:2])
+        link = event.get("bookingLink")
+        movies.append(
+            Screening(format_date, "רב חן", location.value['name'], location.value['dis'], movie_name,
+                      MovieType.m_2D, m_time, link)
+        )
     print("DONE")
 
 
-def get_movies(year: str, month: str, day: str):
+def get_movies(year: str, month: str, day: str, s: requests.Session):
     print("STARTED RAV HEN")
     date = "{}-{}-{}".format(year, month.zfill(2), day.zfill(2))
     format_date = "{}-{}-{}".format(day.zfill(2), month.zfill(2), year)
     for location in Locations:
-        get_by_location(location, date, format_date)
+        get_by_location(location, date, format_date, s)
     print("DONE RAV HEN")
