1a2ced79fbc3ce00cd3a6c4cc5f9970ee0d4a081
tal-sitton <tal.sitton04@gmail.com>
2023-02-24 17:46:05 +0200

    added logs

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import json
+import logging
+import os
+import pathlib
 from datetime import datetime as d, timedelta
 
 import pytz
@@ -37,18 +40,47 @@ def get_all_movies():
         month = str(date.month).zfill(2)
         day = str(date.day).zfill(2)
 
-        yes_planet.get_movies(year, month, day, s)
-        rav_hen.get_movies(year, month, day, s)
-        hot_cinema.get_movies(year, month, day, s)
-        cinema_city.get_movies(year, month, day, s)
-        lev.get_movies(year, month, day, s)
+        try:
+            yes_planet.get_movies(year, month, day, s)
+        except Exception as e:
+            logging.error('Yes Planet crashed!', exc_info=e)
+        try:
+            rav_hen.get_movies(year, month, day, s)
+        except Exception as e:
+            logging.error('Rav Hen crashed!', exc_info=e)
+        try:
+            hot_cinema.get_movies(year, month, day, s)
+        except Exception as e:
+            logging.error('Hot Cinema crashed!', exc_info=e)
+
+        try:
+            cinema_city.get_movies(year, month, day, s)
+        except Exception as e:
+            logging.error('Cinema city crashed!', exc_info=e)
+
+        try:
+            lev.get_movies(year, month, day, s)
+        except Exception as e:
+            logging.error('Lev crashed!', exc_info=e)
         date += timedelta(days=1)
+        break
 
     create_json()
     print(len(movies))
 
 
+def prepare_logs(logs_path):
+    if logs_path.exists():
+        logs_path.unlink()
+
+    file_handler = logging.FileHandler('logs.txt', delay=True)
+    file_handler.setLevel(logging.DEBUG)
+    logging.root.addHandler(file_handler)
+
+
 def main():
+    logs_path = pathlib.Path('logs.txt')
+    prepare_logs(logs_path)
     get_all_movies()
 
 
