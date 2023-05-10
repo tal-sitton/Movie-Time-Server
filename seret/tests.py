4b6fdbf1434b360f830c9cdbc06dedcb604d7765
tal-sitton <tal.sitton04@gmail.com>
2023-05-10 23:22:33 +0300

    added tests

---
+++ seret/tests.py
@@ -0,0 +1,29 @@
+import pytest
+import requests
+
+from .seret_api import get_info
+
+testData = [
+    ("מריו", "האחים סופר מריו: הסרט", "https://www.seret.co.il/images/movies/Mario/Mario1.jpg"),
+    ("צעירים לנצח", "צעירים לנצח", "https://www.seret.co.il/images/movies/ForeverYoung/ForeverYoung1.jpg"),
+    ("הכל בכל מקום בבת אחת", "הכל בכל מקום בבת אחת",
+     "https://www.seret.co.il/images/movies/EverythingEverywhereAllAtOnce/EverythingEverywhereAllAtOnce1.jpg"),
+    ("הסירו דאגה מלבכם", "הסירו דאגה מלבכם",
+     "https://www.seret.co.il/images/movies/ShakeYourCaresAway/ShakeYourCaresAway1.jpg"),
+]
+
+
+@pytest.mark.parametrize("movie_name,expected_name,expected_image_url", testData)
+def test_seret_url(movie_name, expected_name, expected_image_url):
+    session = requests.Session()
+    movie = get_info(session, movie_name)
+    assert movie.name == expected_name
+    assert movie.image_url == expected_image_url
+
+
+def test_invalid_seret_url():
+    session = requests.Session()
+    movie = get_info(session, "מ")
+    assert movie.name == "מ"
+    assert movie.image_url == ""
+    assert movie.rating is None
