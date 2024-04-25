3c8683660b3c1aa11d1b9189441f7f9eaa92d231
tal-sitton <tal.sitton04@gmail.com>
2024-04-25 21:55:35 +0300

    test: update tests

---
+++ tests/test_cinemas.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+import pytest
+import pytz
+import requests
+
+from consts import headers
+from models import cinemas_get_screenings, GetScreeningCallable
+
+date = datetime.now(pytz.timezone('ASIA/TEL_AVIV'))
+year = str(date.year)
+month = str(date.month).zfill(2)
+day = str(date.day).zfill(2)
+
+
+@pytest.mark.parametrize("get_screenings", cinemas_get_screenings.values(), ids=cinemas_get_screenings.keys())
+def test_cinema(get_screenings: GetScreeningCallable):
+    s = requests.session()
+    s.headers.update(headers)
+    assert get_screenings(year, month, day, s)
