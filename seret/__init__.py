7096cfb911c16d5caef913fab08c815abea202a7
tal-sitton <tal.sitton04@gmail.com>
2024-04-28 19:04:11 +0300

    fix: check hebrew name first for seret

--- seret/__init__.py
+++ seret/__init__.py
@@ -5,7 +5,7 @@ ELASTIC_HOST: str = os.environ.get("ELASTIC_HOST") or "http://localhost:9200"
 
 ELASTIC_MOVIES_INDEX = "seret_movies"
 
-ELASTIC_NEEDED_SEARCH_MIN_SCORE = 8
+ELASTIC_NEEDED_SEARCH_MIN_SCORE = 7.8
 
 
 class ElasticSearchField(Enum):
