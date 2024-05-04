9455a573bd51f73f950ecc20ebf4ecd6cf826bf7
tal-sitton <tal.sitton04@gmail.com>
2024-05-04 18:56:56 +0300

    fix: cinemas don't work on memorial days

--- tests/test_cinemas.py
+++ tests/test_cinemas.py
@@ -7,14 +7,18 @@ import requests
 from consts import headers
 from models import cinemas_get_screenings, GetScreeningCallable
 
-date = datetime.now(pytz.timezone('ASIA/TEL_AVIV')) + timedelta(days=1)
-year = str(date.year)
-month = str(date.month).zfill(2)
-day = str(date.day).zfill(2)
-
 
 @pytest.mark.parametrize("get_screenings", cinemas_get_screenings.values(), ids=cinemas_get_screenings.keys())
 def test_cinema(get_screenings: GetScreeningCallable):
     s = requests.session()
     s.headers.update(headers)
-    assert get_screenings(year, month, day, s)
+    screenings = []
+    for delta in range(1, 3):  # Cinemas don't work on memorial days and Yom Kippur
+        date = datetime.now(pytz.timezone('ASIA/TEL_AVIV')) + timedelta(days=delta)
+        year = str(date.year)
+        month = str(date.month).zfill(2)
+        day = str(date.day).zfill(2)
+        screenings = get_screenings(year, month, day, s)
+        if screenings:
+            break
+    assert screenings
